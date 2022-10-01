ddaaab56ce611f817cdf07f67f8beb676600b10b
Ivy Lee <hlee443@my.bcit.ca>
2022-10-01 02:59:50 -0700

    yay

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,35 +1,62 @@
-import database from "./databaseConnection";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  multipleStatements: false,
+  namedPlaceholders: true,
+};
+
+const database = mysql.createPool(dbConfig).promise();
 
 export async function getMoods() {
-	let sqlQuery = "SELECT * FROM moods";
-	const moods = await database.query(sqlQuery);
-	// console.log(moods,"help me")
-	return moods;
+  // console.log("hi");
+  let sqlQuery = "SELECT * FROM moods";
+  const [moods] = await database.query(sqlQuery);
+  // console.log(moods,"help me")
+  return moods;
 }
 
-async function getMood(id) {
-	let sqlQuery = "SELECT * FROM moods WHERE id = ?";
-	const [mood] = await database.query(sqlQuery, [id]);
-	return mood;
+export async function getMood(id) {
+  let sqlQuery = "SELECT * FROM moods WHERE id = ?";
+  // console.log(id);
+  const [mood] = await database.query(sqlQuery, [id]);
+  // console.log([mood]);
+  return mood;
 }
 
-export async function addMood(note, rating, date) {
-	let sqlQuery = "INSERT INTO moods (note, rating, date) VALUES (?,?,?)";
-	const [mood] = await database.query(sqlQuery, [note, rating, date]).then(([result]) => getMood(result.insertId));
-	// console.log([mood],"mood")
-	return mood;
+export async function addMood({ note, rating, date }) {
+  // console.log(note);
+  // console.log(rating);
+  // console.log(date);
+  let sqlQuery = "INSERT INTO moods (note, rating, date) VALUES (?,?,?)";
+  const [mood] = await database
+    .query(sqlQuery, [note, rating, date])
+    .then(([result]) => getMood(result.insertId));
+  // console.log([mood],"mood")
+  return mood;
 }
 
-export async function updateMood(id, note, rating) {
-	let sqlQuery = "UPDATE moods SET note = ?, rating = ? WHERE id = ?";
-	const [mood] = await database.query(sqlQuery, [note, rating, id]).then(() => getMood(id));
-	return mood;
+export async function updateMood({ id, newNote, newRating }) {
+  console.log(newNote);
+  console.log(newRating);
+  console.log(id);
+  let sqlQuery = "UPDATE moods SET note = ?, rating = ? WHERE id = ?";
+  // console.log(id, note, rating);
+  const [mood] = await database
+    .query(sqlQuery, [newNote, newRating, id])
+    .then(() => getMood(id));
+  // console.log(getMood(id));
+  return mood;
 }
 
 export async function deleteMood(id) {
-	let sqlQuery = "DELETE FROM moods WHERE id = ?";
-	const mood = await database.query(sqlQuery, [id]);
-	return mood;
+  let sqlQuery = "DELETE FROM moods WHERE id = ?";
+  const mood = await database.query(sqlQuery, [id]);
+  return mood;
 }
-
-
--- server.js
+++ server.js
@@ -1,66 +1,75 @@
 import express from "express";
 import bodyParser from "body-parser";
 import "dotenv/config";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import {getMoods, addMood, updateMood, deleteMood, getMood} from "./databaseAccessLayer.js";
 dotenv.config();
 const port = process.env.PORT;
 const app = express();
-import * as db from "./databaseConnection.js";
+// import database from "./databaseConnection.js";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("dist"));
 
+app.get("/test", (req, res) => {
+  res.status(200).send({ message: "It's working" });
+  return;
+});
+
 app.get("/api/moods", async (req, res) => {
-    try {
-        // console.log(res.data);
-        const moods = await db.getMoods();
-        res.status(200).json(moods);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  try {
+    const moods = await getMoods();
+    // console.log(moods);
+    res.status(200).json(moods);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.get("/api/moods/:id", async (req, res) => {
-    try {
-        const moods = await db.getMood(req.params.id);
-        res.status(200).json(moods);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  try {
+    const moods = await getMood(req.params.id);
+    res.status(200).json(moods);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // create a new mood
 app.post("/api/moods", async (req, res) => {
-    try {
-        const mood = await db.createMood(req.body);
-        res.status(200).json(mood);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  try {
+    // console.log(req.body);
+    const mood = await addMood(req.body);
+    console.log(mood);
+    res.status(200).json(mood);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // update a mood
 app.put("/api/moods/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const mood = await db.updateMood({...req.body, id});
-        res.status(200).json(mood);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  try {
+    const id = +req.params.id;
+    // console.log(req.body)
+    const mood = await updateMood({ ...req.body, id });
+    res.status(200).json(mood);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // delete a mood
 app.delete("/api/moods/:id", async (req, res) => {
-    try {
-        const moods = await db.deleteMood(req.params.id);
-        res.status(200).json(moods);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  try {
+    const moods = await deleteMood(+req.params.id);
+    res.status(200).json(moods);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
